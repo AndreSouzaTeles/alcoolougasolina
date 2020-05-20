97a5b7fdf3e350363c47b5bbeff59263ddd68caa
Andre Souza Teles <andre.souza.teles@hotmail.com>
2020-05-20 14:49:15 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:alcoolougasolina/AlcoolOuGasolina.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+      MaterialApp(
+      home: AlcoolOuGasolina(),
+      debugShowCheckedModeBanner: false,
+      ) //MaterialApp
+  );
+}
